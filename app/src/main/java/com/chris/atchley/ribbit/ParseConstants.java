d4a7ed73fbd8b897561e5bc76df4f64e874ddb61
Chris Atchley <steve.chris.atchley@gmail.com>
2015-10-22 18:54:26 -0400

    Completed "Capturing Photos and Videos" in treehouse

---
+++ app/src/main/java/com/chris/atchley/ribbit/ParseConstants.java
@@ -0,0 +1,13 @@
+package com.chris.atchley.ribbit;
+
+/**
+ * Created by Chris on 10/19/2015.
+ */
+public final class ParseConstants {
+
+    //Class name
+
+    //Field Names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+}
